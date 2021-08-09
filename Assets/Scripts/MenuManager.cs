cbec67942ab6b8ca9eb9e4c971087bb0ed79271b
Peps <voitko.b@donnu.edu.ua>
2021-08-09 22:33:16 +0300

    спрайти

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // public GameObject buttonPlay;
+    // public GameObject buttonShop;
+    // public GameObject buttonExit;
+    // public GameObject buttonOptions;
+    public GameObject allButtons;
+    
+    
+    
+    
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void SetActiveButtons(bool state)
+    {
+        allButtons.SetActive(state);
+    }
+
+    public void Shop()
+    {
+        SetActiveButtons(false);
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("FirstScene");
+    }
+    
+    
+    
+
+}
