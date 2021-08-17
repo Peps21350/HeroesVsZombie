35a6245298c2053befbc4bce6afc9800da8f771a
Peps <voitko.b@donnu.edu.ua>
2021-08-17 23:02:56 +0300

    Розбито на 2 файли спавн мобів та UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,53 +10,44 @@ namespace DefaultNamespace
     {
         public Text text_health_heroes;
         private static float HeroesHp;
-        public Text text_count_coins;
+        [SerializeField] private Text text_count_coins;
+        [SerializeField] private Button[] buttons_spawn = new Button[3];
         
         private bool isGameStopped = false;
-        private static int timer = 0;
+        [NonReorderable]public static int timer = 0;
         //public event Action<int> onCountCoinChange = delegate {};
 
         public GameObject[] raw_image = new GameObject[4];
-
-
-        public GameObject[] infantryman;
-        public GameObject[] robot;
-        private bool state_spawn = false;
-        private bool type_npc;
-        private bool is_infantryman = false; 
-        private int number_npc;
-        [NonReorderable] private  List<NpcInfantryman> created_infantryman = new List<NpcInfantryman>();
-        [NonReorderable] private List<NpcRobot> created_robot = new List<NpcRobot>();
-
-
+        
         [NonReorderable]public static int count_coins;
         public int count_coins_per_seconds = 2;
-
         
-
         private void  Update()
         {
             SetTextCountCoins();
+            ShowHPHeroes();
             HideCurtain();
         }
-        
-        
-        public void ClickOnButtonWithNPC()
+
+        public void ShowButtonsSpawn(bool state)
         {
-            number_npc = 1;
-            state_spawn = true;
+            if (buttons_spawn[0] != null)
+            {
+                foreach (Button button in buttons_spawn)
+                {
+                    button.gameObject.SetActive(state);
+                }
+            }
+            else
+                Debug.Log("Button array = null");
         }
-        public void ClickOnButtonWithNPC2()
+
+        private void ShowHPHeroes()
         {
-            number_npc = 2;
-            state_spawn = true;
-            
+            float all_health = NpcSpawner.all_health;
+            text_health_heroes.text = $"{(int)all_health}";
         }
         
-        public void OnButtonClickInfantryman()
-        {
-            is_infantryman = true;
-        }
 
         private void HideCurtain()
         {
@@ -79,141 +70,6 @@ namespace DefaultNamespace
         }
         
 
-        private void SpawnInfantryman(Vector3 coords_spawn, int number_npc)
-        {
-            if (number_npc == 1)
-            {
-                count_coins = count_coins - 10;
-                timer = count_coins / 2;
-                Spawn<NpcInfantryman>(coords_spawn,infantryman,0,"Infantryman", 75,400000,10,0,created_infantryman);
-            }
-            else
-            {
-                count_coins = -35;
-                timer = count_coins / 2;
-                Spawn<NpcInfantryman>(coords_spawn,infantryman,1,"Infantryman Hard", 150,300000,35,60,created_infantryman);
-            }
-
-        }
-        
-        private void SpawnRobot(Vector3 coords_spawn, int number_npc)
-        {
-            if (number_npc == 1)
-            {
-                count_coins = -15;
-                timer = count_coins / 2;
-                Spawn<NpcRobot>(coords_spawn, robot, 0, "Robot1", 90, 400000, 15, 15, created_robot);
-            }
-            else
-            {
-                count_coins = -25;
-                timer = count_coins / 2;
-                Spawn<NpcRobot>(coords_spawn,robot,1,"Robot2", 50,400000,25,30,created_robot);
-            }
-
-        }
-
-        
-        
-        public void SpawnNPCUp()
-        {
-            if (state_spawn)
-            {
-                Vector3 result = new Vector3(250f, 554f, 0);
-                if(is_infantryman)
-                    SpawnInfantryman(result,number_npc);
-                else
-                    SpawnRobot(result,number_npc);
-                is_infantryman = false;
-            }
-            state_spawn = false;
-
-        }
-        
-        public void SpawnNPCMidle()
-        {
-            if (state_spawn)
-            {
-                Vector3 result = new Vector3(250f, 420f, 0);
-                if(is_infantryman)
-                    SpawnInfantryman(result,number_npc);
-                else
-                    SpawnRobot(result,number_npc);
-                is_infantryman = false;
-            }
-            state_spawn = false;
-        }
-        
-        public void SpawnNPCDown()
-        {
-            if (state_spawn)
-            {
-                Vector3 result = new Vector3(250f, 290f, 0);
-                if(is_infantryman)
-                    SpawnInfantryman(result,number_npc);
-                else
-                    SpawnRobot(result,number_npc);
-                is_infantryman = false;
-            }
-            state_spawn = false;
-        }
-        
-        
-        public void Spawn<T>(Vector3 result_coords,GameObject[] mass_game_objects,int number_of_hero, string name_hero,float health, float speed, int price_to_spawn, int price, List<T> list) where  T : Npc
-        {
-            T new_hero = Instantiate(mass_game_objects[number_of_hero], result_coords, Quaternion.identity).GetComponent<T>();
-            new_hero.transform.localScale = new Vector3(mass_game_objects[number_of_hero].transform.localScale.x, mass_game_objects[number_of_hero].transform.localScale.y, 1);
-            switch (number_of_hero)
-            {
-                case 0: 
-                    new_hero.init(name_hero, health,speed,price_to_spawn,price);
-                    break;
-                case 1:
-                    new_hero.init(name_hero, health,speed,price_to_spawn,price);
-                    break;
-            }
-            new_hero.GetComponent<Rigidbody2D>().AddForce(new Vector2(Time.deltaTime * new_hero.speedOfMovement,0));
-            new_hero.onHealthChange += _ => AllHPNPC();
-            list.Add(new_hero);
-        }
-        
-        
-        
-        private void AllHPNPC()
-        {
-            float all_health = 0;
-            if (created_infantryman != null)
-            {
-                foreach (var infantryman in created_infantryman)
-                {
-                    all_health += infantryman.currentHealth;
-                }
-            }
-            if (created_robot != null)
-            {
-                foreach (var robot in created_robot)
-                {
-                    all_health += robot.currentHealth;
-                }
-            }
-            text_health_heroes.text = $"{(int)all_health}";
-        }
-
-
-        
-        public static void EndGame()
-        {
-            
-        }
-        public void Pause()
-        {
-            // movement.enabled = false;
-            // gmenu.Open(true,false);
-        }
-        
-        
-       
-
         IEnumerator Timer() 
         {        
             while(true) 
@@ -230,6 +86,7 @@ namespace DefaultNamespace
 
         private void Start() 
         {  
+            ShowButtonsSpawn(false);
             StartCoroutine(Timer());     
         }
         
@@ -238,14 +95,7 @@ namespace DefaultNamespace
             count_coins = +timer * count_coins_per_seconds;
             text_count_coins.text = $"{count_coins}";
         }
-
-
-        public void Finish()
-        {
-            // movement.enabled = false;
-            // NewMothod();
-            // gmenu.Open(false, true);
-        }
+        
     }
     
 }
---
+++ Assets/Scripts/NpcSpawner.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+namespace DefaultNamespace
+{
+    public class NpcSpawner : MonoBehaviour
+    {
+        
+        public GameObject[] infantryman;
+        public GameObject[] robot;
+        private bool state_spawn = false;
+        private bool type_npc;
+        private bool is_infantryman = false; 
+        private int number_npc;
+        public static float all_health = 0; 
+        [NonReorderable] private  List<NpcInfantryman> created_infantryman = new List<NpcInfantryman>();
+        [NonReorderable] private List<NpcRobot> created_robot = new List<NpcRobot>();
+        private GameManager gm = new GameManager();
+
+
+        public void ClickOnButtonWithNPC()
+        {
+            number_npc = 1;
+            state_spawn = true;
+            gm.ShowButtonsSpawn(true);
+        }
+        public void ClickOnButtonWithNPC2()
+        {
+            number_npc = 2;
+            state_spawn = true;
+            gm.ShowButtonsSpawn(true);
+            
+        }
+        
+        public void OnButtonClickInfantryman()
+        {
+            is_infantryman = true;
+        }
+        
+         private void SpawnInfantryman(Vector3 coords_spawn, int number_npc)
+        {
+            if (number_npc == 1)
+            {
+                GameManager.count_coins -= 10;
+                GameManager.timer = GameManager.count_coins / 2;
+                Spawn<NpcInfantryman>(coords_spawn,infantryman,0,"Infantryman", 75,400000,10,0,created_infantryman);
+            }
+            else
+            {
+                GameManager.count_coins -= 35;
+                GameManager.timer = GameManager.count_coins / 2;
+                Spawn<NpcInfantryman>(coords_spawn,infantryman,1,"Infantryman Hard", 150,300000,35,60,created_infantryman);
+            }
+
+        }
+        
+        private void SpawnRobot(Vector3 coords_spawn, int number_npc)
+        {
+            if (number_npc == 1)
+            {
+                GameManager.count_coins -= 15;
+                GameManager.timer = GameManager.count_coins / 2;
+                Spawn<NpcRobot>(coords_spawn, robot, 0, "Robot1", 90, 400000, 15, 15, created_robot);
+            }
+            else
+            {
+                GameManager.count_coins -= 25;
+                GameManager.timer = GameManager.count_coins / 2;
+                Spawn<NpcRobot>(coords_spawn,robot,1,"Robot2", 50,400000,25,30,created_robot);
+            }
+
+        }
+        
+        public void SpawnNPCUp()
+        {
+            if (state_spawn)
+            {
+                Vector3 result = new Vector3(250f, 554f, 0);
+                if(is_infantryman)
+                    SpawnInfantryman(result,number_npc);
+                else
+                    SpawnRobot(result,number_npc);
+                is_infantryman = false;
+                gm.ShowButtonsSpawn(false);
+            }
+            state_spawn = false;
+
+        }
+        
+        public void SpawnNPCMidle()
+        {
+            if (state_spawn)
+            {
+                Vector3 result = new Vector3(250f, 420f, 0);
+                if(is_infantryman)
+                    SpawnInfantryman(result,number_npc);
+                else
+                    SpawnRobot(result,number_npc);
+                is_infantryman = false;
+                gm.ShowButtonsSpawn(false);
+            }
+            state_spawn = false;
+        }
+        
+        public void SpawnNPCDown()
+        {
+            if (state_spawn)
+            {
+                Vector3 result = new Vector3(250f, 290f, 0);
+                if(is_infantryman)
+                    SpawnInfantryman(result,number_npc);
+                else
+                    SpawnRobot(result,number_npc);
+                is_infantryman = false;
+                gm.ShowButtonsSpawn(false);
+            }
+            state_spawn = false;
+        }
+        
+        
+        public void Spawn<T>(Vector3 result_coords,GameObject[] mass_game_objects,int number_of_hero, string name_hero,float health, float speed, int price_to_spawn, int price, List<T> list) where  T : Npc
+        {
+            T new_hero = Instantiate(mass_game_objects[number_of_hero], result_coords, Quaternion.identity).GetComponent<T>();
+            new_hero.transform.localScale = new Vector3(mass_game_objects[number_of_hero].transform.localScale.x, mass_game_objects[number_of_hero].transform.localScale.y, 1);
+            switch (number_of_hero)
+            {
+                case 0: 
+                    new_hero.init(name_hero, health,speed,price_to_spawn,price);
+                    break;
+                case 1:
+                    new_hero.init(name_hero, health,speed,price_to_spawn,price);
+                    break;
+            }
+            new_hero.GetComponent<Rigidbody2D>().AddForce(new Vector2(Time.deltaTime * new_hero.speedOfMovement,0));
+            new_hero.onHealthChange += _ => AllHPNPC();
+            list.Add(new_hero);
+        }
+        
+
+        private float AllHPNPC()
+        {
+            //float all_health = 0;
+            float all_health_infantryman = 0;
+            float all_health_robot = 0;
+            if (created_infantryman != null)
+            {
+                foreach (var infantryman in created_infantryman)
+                {
+                    all_health_infantryman += infantryman.currentHealth;
+                }
+            }
+            if (created_robot != null)
+            {
+                foreach (var robot in created_robot)
+                {
+                    all_health_robot += robot.currentHealth;
+                }
+            }
+
+            return all_health = all_health_infantryman + all_health_robot;
+
+        }
+        
+    }
+}
